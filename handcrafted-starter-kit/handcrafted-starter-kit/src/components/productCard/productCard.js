c9fc30e671c5d34877422a26bf79efaf9900431d
AlexwKimotho <lexkimothowachira@gmail.com>
2023-12-18 11:08:44 +0300

    Update: Added Handcrfted files

--- handcrafted-starter-kit/handcrafted-starter-kit/src/components/productCard/productCard.js
+++ handcrafted-starter-kit/handcrafted-starter-kit/src/components/productCard/productCard.js
@@ -12,6 +12,33 @@ export function productCard(props) {
         </p>
         <p class="product-price">$19.99</p>
       </div>
+
+      <div class="product_card">
+      <img src="${iphone}" alt="Product Image" class="product-image"/>
+      <h2 class="product-title">Product Title</h2>
+      <p class="product-description">
+        Product Description Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      </p>
+      <p class="product-price">$19.99</p>
+    </div>
+
+    <div class="product_card">
+    <img src="${iphone}" alt="Product Image" class="product-image"/>
+    <h2 class="product-title">Product Title</h2>
+    <p class="product-description">
+      Product Description Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    </p>
+    <p class="product-price">$19.99</p>
+  </div>
+
+  <div class="product_card">
+  <img src="${iphone}" alt="Product Image" class="product-image"/>
+  <h2 class="product-title">Product Title</h2>
+  <p class="product-description">
+    Product Description Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+  </p>
+  <p class="product-price">$19.99</p>
+</div>
     </div>
   `;
 }
