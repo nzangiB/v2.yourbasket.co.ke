9bd1f311484bb08803d4d423b0a94856f629197b
Michael Mwaura <michaelwmwaura@gmail.com>
2024-03-08 00:41:59 +0300

    Data services now has fallback to prod

--- src/services/data.service.js
+++ src/services/data.service.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import AuthService from "./auth.service";
 
+// const API_URL = (process.env.NODE_ENV === "development" ? "http://localhost:3000/" : "https://api.yourbasket.co.ke/");
 const API_URL = "https://api.yourbasket.co.ke/";
 
 axios.interceptors.request.use(function (config) {
