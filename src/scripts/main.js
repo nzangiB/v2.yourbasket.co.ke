96a14c25b5ee1aae704ab6aa8d394fc757f9c24b
Michael Mwaura <michaelwmwaura@gmail.com>
2024-03-10 17:54:36 +0300

    fixup! Move login and register pages to pages/.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -13,8 +13,8 @@ import Product from "../pages/product/product";
 import Cart from "../pages/cart";
 import Checkout from "../pages/checkout";
 import Categories from "../pages/categories/categories";
-import Login from "../pages/account/login";
-import Register from "../pages/account/register";
+import Login from "../pages/login";
+import Register from "../pages/register";
 import Account from "../pages/account/account";
 
 // initialize defaults
@@ -29,26 +29,8 @@ const routes = [
 	 */
   {
     path: "/",
-    data: { page: { title: "Home" } },
-    template: Home,
-    controller: () => {
-      const Flickity = window.Flickity;
-      const carousels = document.querySelectorAll(".carousel");
-      for (const carousel of carousels) {
-        new Flickity(carousel, {
-          autoPlay: true,
-          // arrowShape: "M0.749603 9.44518C0.821801 9.51929 1.09441 9.83618 1.34835 10.0968C2.83712 11.7362 6.72086 14.4196 8.75361 15.2386C9.06231 15.3702 9.8428 15.6488 10.2598 15.6667C10.6594 15.6667 11.0403 15.5747 11.4038 15.3881C11.8569 15.1275 12.2203 14.7173 12.4195 14.233C12.5477 13.897 12.7469 12.8913 12.7469 12.8735C12.9461 11.7733 13.0556 9.98568 13.0556 8.01023C13.0556 6.12807 12.9461 4.41329 12.783 3.29651C12.7643 3.27862 12.5652 2.02895 12.3473 1.6009C11.9477 0.818899 11.1673 0.333344 10.332 0.333344H10.2598C9.71583 0.35251 8.57187 0.838066 8.57187 0.855955C6.64867 1.67629 2.85454 4.22801 1.32968 5.92362C1.32968 5.92362 0.900223 6.35934 0.713504 6.63151C0.422223 7.02251 0.277828 7.50679 0.277828 7.99107C0.277828 8.53157 0.440895 9.03501 0.749603 9.44518",
-          cellAlign: "left",
-          // imagesLoaded: true,
-          // percentagePosition: true,
-          lazyLoad: true,
-          contain: true
-          // prevNextButtons: true,
-          // pageDots: false
-          // watchCSS: true
-        });
-      }
-    }
+    data: { page: { title: "Online Shopping for Furniture, Appliances & More!" } },
+    template: Home
   },
   /**
 	 * 02 - Product
