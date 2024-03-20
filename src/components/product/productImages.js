a2cf72dd33932bbb83f9d49a6d15cb256d412659
Michael Mwaura <michaelwmwaura@gmail.com>
2024-03-20 18:49:43 +0300

    Merge productImages and ProductImageSlider

--- src/components/product/productImages.js
+++ src/components/product/productImages.js
@@ -1,9 +1,116 @@
 import { useState } from "../../plugins/react";
-
-import ProductImageSlider from "./ProductImageSlider";
+import placeholder from "../../assets/images/placeholder.png";
 
 import "./productImages.scss";
-import placeholder from "../../assets/images/placeholder.png";
+
+const slideStyles = {
+  width: "100%",
+  height: "100%",
+  borderRadius: "10px",
+  backgroundSize: "contain",
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat"
+};
+
+const arrowStyles = {
+  fontSize: "18px",
+  color: "#000000",
+  cursor: "pointer"
+};
+
+const sliderStyles = {
+  position: "relative",
+  height: "100%"
+};
+
+const dotsContainerStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center"
+};
+
+const dotStyle = {
+  margin: "0 3px",
+  cursor: "pointer",
+  fontSize: "15px"
+};
+
+function ProductImageSlider ({ currentImage, slides }) {
+  if (!slides.length) {
+    return (
+      <div className="product-images__slider">
+        <img src={currentImage} alt="product image" className={"img"}/>
+      </div>
+    );
+  }
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const goToPrevious = () => {
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToNext = () => {
+    const isLastSlide = currentIndex === slides.length - 1;
+    const newIndex = isLastSlide ? 0 : currentIndex + 1;
+    setCurrentIndex(newIndex);
+  };
+
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
+  };
+
+  const slideStylesWithBackground = {
+    ...slideStyles,
+    backgroundImage: `url("${slides[currentIndex].url}")`
+  };
+
+  return (
+    <div className="product-images__slider">
+      <div style={sliderStyles}>
+        <div style={slideStylesWithBackground}></div>
+        {slides.length > 1 && <div style={dotsContainerStyles}>
+          <button onClick={goToPrevious} style={{ ...arrowStyles, marginRight: "50px" }}>
+            <span>❰</span>
+          </button>
+          {slides.map((slide, slideIndex) => (
+            <div
+              style={{
+                ...dotStyle,
+                color: currentIndex === slideIndex ? "black" : "gray"
+              }}
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+            >
+              <span>●</span>
+            </div>
+          ))}
+          <button onClick={goToNext} style={{ ...arrowStyles, marginLeft: "50px" }}>
+            <span>❱</span>
+          </button>
+        </div>}
+      </div>
+    </div>
+  );
+}
+
+function ProductImageThumbnails ({ images, setCurrentImage }) {
+  return (
+    <section className="product-image__thumbnails">
+      <ul>
+        {images.length && images.map(image => (
+          <li>
+            <button onClick={() => setCurrentImage(image)}>
+              <img src={image} alt="product thumbnail"/>
+            </button>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
 
 export function ProductImages ({ product }) {
   const [currentImage, setCurrentImage] = useState(product.file_path
@@ -18,37 +125,11 @@ export function ProductImages ({ product }) {
     const title = "product image";
     return ({ url, title });
   });
-  const containerStyles = {
-    width: "100%",
-    aspectRatio: "4/3",
-    margin: "0 auto"
-  };
-
-  function ImageThumbnails (images) {
-    return (
-      <section className="image-thumbnails">
-        <ul>
-          {images.length && images.map(image => (
-            <li>
-              <button onClick={() => setCurrentImage(image)}>
-                <img src={image} alt="product thumbnail"/>
-              </button>
-            </li>
-          ))}
-        </ul>
-      </section>
-    );
-  }
 
   return (
     <section className="product-images">
-      <div style={containerStyles}>
-        {slides.length
-          ? <ProductImageSlider slides={slides}/>
-          : <img src={currentImage} alt="product image" className={"img"}/>
-        }
-      </div>
-      <ImageThumbnails images={product.images}/>
+      <ProductImageSlider currentImage={currentImage} slides={slides}/>
+      <ProductImageThumbnails images={product.images} setCurrentImage={setCurrentImage}/>
     </section>
   );
 }
