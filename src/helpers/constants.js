79bdeb1def028e9158e10cc49a65954fcaf7eaa2
Michael Mwaura <michaelwmwaura@gmail.com>
2024-04-04 13:58:25 +0300

    Update adult content categories

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,1 +1,4 @@
 export const API_URL = (process.env.NODE_ENV === "development" ? "http://localhost:3000/" : "https://api.yourbasket.co.ke/");
+export const CATEGORIES_ADULT_CONTENT = [
+  "drinks--alcohol"
+];
