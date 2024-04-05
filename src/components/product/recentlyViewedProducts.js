01162a4f8b3a9df28653ab1ad7617465d22049d9
Michael Mwaura <michaelwmwaura@gmail.com>
2024-04-05 12:11:53 +0300

    fixup! Fix issue with site being responsive after compressed product cards

--- src/components/product/recentlyViewedProducts.js
+++ src/components/product/recentlyViewedProducts.js
@@ -2,21 +2,25 @@ import HelperService from "../../services/helper.service";
 
 import { ProductRow } from "../productCard/productCards";
 
+import "./recentlyViewedProducts.scss";
+
 export async function RecentlyViewedProducts ({ product }) {
   const recentProducts = HelperService.getRecentProducts();
   HelperService.setRecentProducts(product);
 
   return (
-    <div className="deals-grid deals-recommended">
-      <header className="deals__header">
-        <div className="deals__title">
-          <h3 className={"title"}>Recently Viewed</h3>
-        </div>
-      </header>
+    <div className="recently-viewed-products">
+      <div className="deals-grid">
+        <header className="deals__header">
+          <div className="deals__title">
+            <h3 className={"title"}>Recently Viewed</h3>
+          </div>
+        </header>
 
-      <section className="deals__list">
-        <ProductRow products={recentProducts.slice(0, 4)}/>
-      </section>
+        <section className="deals__list">
+          <ProductRow products={recentProducts.slice(0, 4)}/>
+        </section>
+      </div>
     </div>
   );
 }
