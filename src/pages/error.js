f5b6281a1e905f52f0acc44b0d9ca4f5e784cb2f
Michael Mwaura <michaelwmwaura@gmail.com>
2024-04-15 18:00:02 +0300

    Fix issue with error page not matching root styles

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,14 +1,27 @@
 import Layout from "./_layout";
+import { Footer } from "../components/footer/footer";
+import { Header } from "../components/header/header";
 
 import "./error.scss";
 
 function Error404 (props) {
   return (
     <Layout {...props}>
-      <div>
-        <h1 className={"title"}>Error</h1>
-        <p>Page Not Found</p>
+      <Header {...props}/>
+      <div className="container">
+        <main className="content" style={{
+          display: "flex",
+          placeContent: "center",
+          textAlign: "center",
+          padding: "20px"
+        }}>
+          <div className="error">
+            <h1 className={"title"}>Error</h1>
+            <p>Page Not Found</p>
+          </div>
+        </main>
       </div>
+      <Footer/>
     </Layout>
   );
 }
