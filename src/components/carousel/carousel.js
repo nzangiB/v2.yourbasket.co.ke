47b8242248ee7dc438357c2ec56b6c3475808667
Michael Mwaura <michaelwmwaura@gmail.com>
2024-04-22 07:23:38 +0300

    Fix issue with slider not showing consistently

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,109 +1,129 @@
-import placeholder from "../../assets/images/placeholder.png";
+import placeholder from '../../assets/images/placeholder.png';
 
-import "./carousel.scss";
-import { Component } from "@wearearchangel/handcrafted";
+import { Component } from '@wearearchangel/handcrafted';
+
+import './carousel.scss';
 
 export function Slide (banner) {
-  const image = banner.file_path
-    ? "https://api.yourbasket.co.ke/" + banner.file_path
-    : "../assets/img/slider/slider-1.webp";
+	const image = banner.file_path
+		? 'https://api.yourbasket.co.ke/' + banner.file_path
+		: '../assets/img/slider/slider-1.webp';
 
-  let href = "#";
-  if (banner.url) {
-    const url = new URL(banner.url);
-    href = new URL(url.hash.replace("#", "/products"), location.origin);
-  }
+	let href = '#';
+	if (banner.url) {
+		const url = new URL(banner.url);
+		href = new URL(url.hash.replace('#', '/products'), location.origin);
+	}
 
-  return (
-    <a className="banner" href={href}>
-      <div className="banner__img">
-        {/* <img src={image} alt={banner.title} className="img"/> */}
-        <img src={placeholder} data-flickity-lazyload={image} alt={banner.title} className="img"/>
-      </div>
-      <div className="srt banner__text">
-        <h4 className="">{banner.title}</h4>
-        <p className="">{banner.description}</p>
-      </div>
-    </a>
-  );
+	return (
+		<a className="banner" href={href}>
+			<div className="banner__img" style={{
+				backgroundImage: `url(${placeholder})`,
+				backgroundPosition: 'center',
+				backgroundSize: 'cover',
+			}}>
+				<img src={image} alt={banner.title} className="img"/>
+			</div>
+			<div className="srt banner__text">
+				<h4 className="">{banner.title}</h4>
+				<p className="">{banner.description}</p>
+			</div>
+		</a>
+	);
 }
 
 class Carousel extends Component {
-  template () {
-    const { banners } = this.props;
-    if (!banners.length) {
-      return (
-        <div className="banner">
-          <div className="banner__img"></div>
-          <div className="banner__text">
-            <h4 className="title">No images found</h4>
-          </div>
-        </div>
-      );
-    }
+	template () {
+		const { banners } = this.props;
+		if (!banners.length) {
+			return (
+				<div className="banner">
+					<div className="banner__img"></div>
+					<div className="banner__text">
+						<h4 className="title">No images found</h4>
+					</div>
+				</div>
+			);
+		}
 
-    return (
-      <>
-        {banners.map(Slide).filter(Boolean)}
-      </>
-    );
-  }
+		return (
+			<>
+				{banners.map(Slide).filter(Boolean)}
+			</>
+		);
+	}
 
-  controller ({ component }) {
-    if (!component) component = document.getElementById(this.id);
-    const Flickity = window.Flickity;
-    new Flickity(component, {
-      autoPlay: true,
-      cellAlign: "left",
-      imagesLoaded: true,
-      // percentagePosition: true,
-      lazyLoad: true,
-      contain: true,
-      prevNextButtons: false,
-      // pageDots: true,
-      watchCSS: true
-    });
-  }
+	controller ({ component }) {
+		if (!component) component = document.getElementById(this.id);
+		const jQuery = window.jQuery;
+		if (jQuery) {
+			jQuery(document).ready(function () {
+				jQuery(component).slick({
+					autoplay: true,
+					autoplaySpeed: 5000,
+					arrows: false,
+					dots: true,
+					infinite: true,
+					speed: 500,
+					slidesToShow: 1,
+					slidesToScroll: 1
+				});
+			});
+		}
+		// const Flickity = window.Flickity;
+		// new Flickity(component, {
+		// 	autoPlay: true,
+		// 	cellAlign: 'left',
+		// 	wrapAround: true,
+		// 	imagesLoaded: true,
+		// 	// percentagePosition: true,
+		// 	// lazyLoad: true,
+		// 	contain: true,
+		// 	prevNextButtons: false,
+		// 	pageDots: true,
+		// 	watchCSS: true
+		// });
+	}
 }
 
 export function MainBanner ({ banners }) {
-  return (
-    <div className="main-banner">
-      <Carousel banners={banners}/>
-    </div>
-  );
+	return (
+		<div className="main-banner">
+			<Carousel banners={banners}/>
+		</div>
+	);
 }
 
 export function SideBanner ({ banners }) {
-  return (
-    <div className="side-banner">
-      <Carousel banners={banners}/>
-    </div>
-  );
+	return (
+		<div className="side-banner">
+			<Carousel banners={banners}/>
+		</div>
+	);
 }
 
 export function OfferBanner ({ banners }) {
-  return (
-    <div className="offer-banner">
-      <Carousel banners={banners}/>
-    </div>
-  );
+	return (
+		<div className="offer-banner">
+			<Carousel banners={banners}/>
+		</div>
+	);
 }
 
 export function VendorBanner ({ banners }) {
-  return (
-    <div className="vendor-banner">
-      <Carousel banners={banners}/>
-    </div>
-  );
+	return (
+		<div className="vendor-banner">
+			<Carousel banners={banners}/>
+		</div>
+	);
 }
 
 export function CategoryBanner ({ banners }) {
-  return (
-    <div className="category-banner">
-      <Carousel banners={banners}/>
-    </div>
-  );
+	return (
+		<div className="category-banner">
+			<Carousel banners={banners}/>
+		</div>
+	);
 }
 
 /*
